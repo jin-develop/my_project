436f6913f652317706baa8fd076c7c82168a3d32
jin-develop <jin-develop@users.noreply.github.com>
2020-06-01 20:24:36 +0900

    13

---
+++ gopy/test.py
@@ -0,0 +1,7 @@
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+from travel import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
---
+++ youtube_shuffle/youtube.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+from flask import Flask, render_template, jsonify, request
+
+
+
