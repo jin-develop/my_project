64116f2cdaaab17ceea1d8a96f728df6229abca6
jin-develop <jin-develop@users.noreply.github.com>
2020-06-20 20:04:47 +0900

    separated project
    move to another repo.

--- youtube_shuffle/read.py
+++ youtube_shuffle/read.py
@@ -1,13 +1,10 @@
-import docx
- 
-def ReadingTextDocuments(filename):
-    doc = docx.Document(filename)
+a = 10 * 60 + 10
+print(a)
+total10 = 24 * 60
 
-    completed = []
+percent10 = a/total10
+print(percent10)
 
-    for p in doc.paragraphs:
-        completed.append(p.text)
-
-    return '\n'.join(completed)
-
-print(ReadingTextDocuments("read1.docx"))
+b = 20 * 100
+percent24 = b*percent10
+print(percent24)
--- youtube_shuffle/새 폴더/templates/you.js
+++ youtube_shuffle/새 폴더/templates/you.js
@@ -43,23 +43,29 @@ for (var i = 0 ; i <links.length; i++) {
     hrefs.push(links[i]);
 }
 
-//
-{/* <div id="textHtml2" name="textHtml2" style="width:400px;height:200px;border:solid 1px gray" contenteditable="true"><p class="MsoNormal"><span lang="EN-US"><a href="https://www.youtube.com/watch?v=-3LugpXuG-g&amp;t=1s"><span style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
+// 형식들
+
+/* <div id="textHtml2" name="textHtml2" style="width:400px;height:200px;border:solid 1px gray" contenteditable="true"><p class="MsoNormal"><span lang="EN-US"><a href="https://www.youtube.com/watch?v=-3LugpXuG-g&amp;t=1s"><span style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
 background:#F9F9F9">00:01</span></a></span><span class="style-scope"><span lang="EN-US" style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
 color:#030303;border:none windowtext 1.0pt;mso-border-alt:none windowtext 0cm;
 padding:0cm;background:#F9F9F9"> Stephanie Poetry - I Love You 3000 <o:p></o:p></span></span></p><p class="MsoNormal"><span lang="EN-US"><a href="https://www.youtube.com/watch?v=-3LugpXuG-g&amp;t=210s"><span style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
 background:#F9F9F9">03:30</span></a></span><span class="style-scope"><span lang="EN-US" style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
 color:#030303;border:none windowtext 1.0pt;mso-border-alt:none windowtext 0cm;
 padding:0cm;background:#F9F9F9"> Khai Dreams - Fantasy <o:p></o:p></span></span></p><p class="MsoNormal">
+*/
+
+localStorage.setItem('object', { a: 'b' });
+localStorage.getItem('object'); // [object Object]
+
+localStorage.setItem('object', JSON.stringify({ a: 'b' }));
+JSON.parse(localStorage.getItem('object')); // { a: 'b' }
+
+// href 의 값들을 저장하고. 그 값들을 불러와 함수 실행
 
 
 
-</p><p class="MsoNormal"><span lang="EN-US"><a href="https://www.youtube.com/watch?v=-3LugpXuG-g&amp;t=380s"><span style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
-background:#F9F9F9">06:20</span></a></span><span class="style-scope"><span lang="EN-US" style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
-color:#030303;border:none windowtext 1.0pt;mso-border-alt:none windowtext 0cm;
-padding:0cm;background:#F9F9F9"> </span></span><span class="style-scope"><span style="font-size:10.5pt;line-height:107%;mso-ascii-font-family:Arial;
-mso-hansi-font-family:Arial;mso-bidi-font-family:Arial;color:#030303;
-border:none windowtext 1.0pt;mso-border-alt:none windowtext 0cm;padding:0cm;
-background:#F9F9F9">지다</span></span><span class="style-scope"><span lang="EN-US" style="font-size:10.5pt;line-height:107%;font-family:&quot;Arial&quot;,sans-serif;
-color:#030303;border:none windowtext 1.0pt;mso-border-alt:none windowtext 0cm;
-padding:0cm;background:#F9F9F9"> (Jida) - Blind (feat. Rachel Lim)&nbsp;<o:p></o:p></span></span></p></div> */}
+// 사이트 열었다 끄기 
+var new_popup = window.open('http://www.daum.net ', '_blank'); 
+new_popup.close();
+
+// 타이머 - https://dororongju.tistory.com/68
